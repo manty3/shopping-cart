1098044a357c4950ae3d6cbb4b2a41853a7d0cba
sakthi_l_u_c_i <sakthivel8725@outlook.com>
2024-08-13 13:05:14 +0530

    Fix : Connection issue with mangodb atlas

--- app.js
+++ app.js
@@ -1,71 +1,80 @@
-var createError = require('http-errors');
-var express = require('express');
-const path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var usersRouter = require('./routes/users');
-var adminRouter = require('./routes/admin');
-const hbs=require('express-handlebars')
+var createError = require("http-errors");
+var express = require("express");
+const path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var usersRouter = require("./routes/users");
+var adminRouter = require("./routes/admin");
+const hbs = require("express-handlebars");
 const app = express();
-const fileUpload =require('express-fileupload')
-var db=require('./config/connection');
-var session = require('express-session');
-const router = require('./routes/users');
-require('dotenv').config();
-const MongoStore = require('connect-mongo')
+const fileUpload = require("express-fileupload");
+var db = require("./config/connection");
+var session = require("express-session");
+const router = require("./routes/users");
+require("dotenv").config();
+const MongoStore = require("connect-mongo");
 
 // const { error, log } = require('console');
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials' }))
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutDir: __dirname + "/views/layout/",
+    partialsDir: __dirname + "/views/partials",
+  })
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload())
+app.use(express.static(path.join(__dirname, "public")));
+app.use(fileUpload());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(session({secret:"key",cookie:{maxAge:600000},
-  resave: false,
-  saveUninitialized:true,
-  store: MongoStore.create({
+app.use(
+  session({
+    secret: "key",
+    cookie: { maxAge: 600000 },
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URL_LOCAL,
+    }),
+  })
+);
 
-    mongoUrl:"mongodb://localhost:27017/shopping"
-})
+db.connect((err) => {
+  if (err) console.log("connection error" + err);
 
-}))
-db.connect((err)=>{
-  if(err) console.log("connection error"+err)
+  console.log("Database connected");
+});
 
-  console.log("Database connected")
-})
-
-app.use('/', usersRouter);
-app.use('/admin', adminRouter);
+app.use("/", usersRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
-  console.log('listening on port 3000')
-})
-
+app.listen(PORT, () => {
+  console.log("listening on port 3000");
+});
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
 
 module.exports = router;
--- config/connection.js
+++ config/connection.js
@@ -1,42 +1,29 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const state = {
-
-  db:null
-
-  
+  db: null,
 };
 
-// MongoDB connection string from environment variable
+const URL = process.env.MONGODB_URL_LOCAL;
 
-
-const URL= process.env.MONGODB_URL_LOCAL + "/shopping"
-
-
-if (!URL) {
-  console.error('MONGODB_URL_LOCAL environment variable is not set.');
-  process.exit(1);
-}
-
-console.log("MongoDB URL:", URL);
-
-// Function to establish MongoDB connection using mongoose
 const connect = async () => {
   try {
+    console.log("MongoDB URL:", URL);
     const connection = await mongoose.connect(URL);
-    state.db = connection;
-    
-    console.log('Database connection established using mongoose');
+    state.db = connection.connection.db; // <-- Add this line to correctly set the db object
+
+    console.log("Database connection established using mongoose");
   } catch (err) {
-    console.error('Failed to connect to the database using mongoose. Error:', err);
+    console.error(
+      "Failed to connect to the database using mongoose. Error:",
+      err
+    );
   }
 };
 
-// Function to get the database instance
 const get = () => state.db;
 
-// Exporting functions
 module.exports = {
   connect,
   get,
